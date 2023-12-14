08afee3eb7566777f60cf609d3b954a97ed9915c
Adrian <adrianbueno095@gmail.com>
2023-12-14 16:31:26 +0100

    solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
+import { useState } from 'react';
 import "./App.css";
 import Counter from "./components/Counter";
-import ExponentTwo from "./components/ExponentTwo";
-import ExponentThree from "./components/ExponentThree";
-import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
+import ExponentFour from "./components/ExponentFour";
 import ExponentSix from "./components/ExponentSix";
+import ExponentThree from "./components/ExponentThree";
+import ExponentTwo from "./components/ExponentTwo";
 
+function App() {
+  const [count, setCount] = useState(0);
+  // const [activeIndex, setActiveIndex] = useState(0);
+  const handleIncrement = () => setCount((prevCount) => prevCount + 1)
+  const handleDecrement = () => setCount((prevCount) => prevCount - 1)
+  const result = count;
 
-function App () {
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
-  
-      <Counter/>
+
+      <Counter count={result} decrement={handleDecrement} increment={handleIncrement} />
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+        <ExponentTwo count={result} />
+        <ExponentThree count={result} />
+        <ExponentFour count={result} />
+        <ExponentFive count={result} />
+        <ExponentSix count={result} />
       </div>
     </div>
   );
